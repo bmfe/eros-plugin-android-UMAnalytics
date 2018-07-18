34d7e48081819b3e9e707184844fa6251322043a
liuyuanxiao <liuyuanxiao@benmu-health.com>
2018-07-18 15:17:42 +0800

    first commit

---
+++ src/main/java/com/benmu/umeng/UmengModule.java
@@ -0,0 +1,47 @@
+package com.benmu.umeng;
+
+import android.content.Context;
+import android.text.TextUtils;
+
+import com.alibaba.weex.plugin.annotation.WeexModule;
+import com.benmu.framework.utils.DebugableUtil;
+import com.taobao.weex.annotation.JSMethod;
+import com.taobao.weex.common.WXModule;
+import com.umeng.analytics.MobclickAgent;
+import com.umeng.commonsdk.UMConfigure;
+
+/**
+ * Umeng 埋点 Module
+ * Created by liuyuanxiao on 2018/7/18.
+ */
+@WeexModule(name = "bmUMAnalytics", lazyLoad = true)
+public class UmengModule extends WXModule {
+    @JSMethod
+    public void initUM(String umengAppKey) {
+        Context context = mWXSDKInstance.getContext();
+        if (!TextUtils.isEmpty(umengAppKey)) {
+            UMConfigure.init(context, umengAppKey, "umeng", UMConfigure.DEVICE_TYPE_PHONE, "");
+            MobclickAgent.setDebugMode(DebugableUtil.isDebug());
+            MobclickAgent.openActivityDurationTrack(false);
+            MobclickAgent.setCatchUncaughtExceptions(!DebugableUtil.isDebug());
+            MobclickAgent.setScenarioType(context, MobclickAgent.EScenarioType.E_UM_NORMAL);
+        }
+    }
+
+    @JSMethod(uiThread = true)
+    public void umengOnActivityResume(String name) {
+        UmengUtils.umengOnActivityResume(mWXSDKInstance.getContext(), name);
+    }
+
+    @JSMethod(uiThread = true)
+    public void umengOnActivityPause(String name) {
+        UmengUtils.umengOnActivityPause(mWXSDKInstance.getContext(), name);
+    }
+
+    @JSMethod(uiThread = true)
+    public void umengClick(String name) {
+        UmengUtils.umengClick(mWXSDKInstance.getContext(), name);
+    }
+
+
+}
---
+++ src/main/java/com/benmu/umeng/UmengUtils.java
@@ -0,0 +1,76 @@
+package com.benmu.umeng;
+
+import android.content.Context;
+import android.support.v4.app.Fragment;
+
+import com.umeng.analytics.MobclickAgent;
+
+/**
+ * Created by liuyuanxiao on 2017/1/22.
+ */
+
+public class UmengUtils {
+    /**
+     * activity的友盟统计 & 访问时长,放在onResume方法里,注意不要重复添加此方法,并且如果父类调用了此方法,那么子类就不能在调用
+     * 这个方法了
+     *
+     * @param activity 当前activity对象,传this
+     * @param name     当前activity的页面描述,目前传空就好了
+     */
+    public static void umengOnActivityResume(Context activity, String name) {
+        if (activity == null)
+            return;
+
+        if (name != null) {
+            umengClick(activity, name);
+        }
+        MobclickAgent.onPageStart((name == null) ? activity.getClass().getName() : name);
+        MobclickAgent.onResume(activity);
+    }
+
+    /**
+     * activity的友盟统计 & 访问时长,放在onPause方法里,注意不要重复添加此方法,并且如果父类调用了此方法,那么子类就不能在调用
+     * 这个方法了
+     *
+     * @param activity 当前activity对象,传this
+     * @param name     当前activity的页面描述,目前传空就好了
+     */
+    public static void umengOnActivityPause(Context activity, String name) {
+        if (activity == null)
+            return;
+        MobclickAgent.onPageEnd((name == null) ? activity.getClass().getName() : name);
+        MobclickAgent.onPause(activity);
+    }
+
+    /**
+     * fragment的友盟统计 & 访问时长,放在fragment的onResume中,这个方法不要重复添加,如果父类调用了此方法,那么子类无需再次调用
+     *
+     * @param fragment 传单前的fragment,传this就好了
+     * @param name     目前传空
+     */
+    public static void umengOnFragmentResume(Fragment fragment, String name) {
+        if (fragment == null)
+            return;
+        MobclickAgent.onPageStart((name == null) ? fragment.getClass().getName() : name);
+    }
+
+    /**
+     * fragment的友盟统计 & 访问时长,放在fragment的onPause中,这个方法不要重复添加,如果父类调用了此方法,那么子类无需再次调用
+     *
+     * @param fragment 传单前的fragment,传this就好了
+     * @param name     目前传空
+     */
+    public static void umengOnFragmentPause(Fragment fragment, String name) {
+        if (fragment == null)
+            return;
+        MobclickAgent.onPageEnd((name == null) ? fragment.getClass().getName() : name);
+    }
+
+    /**
+     * @param context
+     * @param pointName 需要埋的点(点击事件）的描述,写在Umeng类中.
+     */
+    public static void umengClick(Context context, String pointName) {
+        MobclickAgent.onEvent(context, pointName);
+    }
+}
